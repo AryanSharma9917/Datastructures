2c2c04744f62bd6a2ec68fb7b73daf54534326aa
Aryan Sharma <sharmaisgreat46@gmail.com>
2022-07-15 02:00:19 +0530

    @Questions

--- Array/Main.java
+++ Array/Main.java
@@ -1,6 +1,24 @@
-public class Main{
-    public static void main(String args[])
+//package com.Aryan;
+import java.util.Scanner; //m n array of m integers
+public class Main
+{
+    public static void main(String[] args)
     {
-        System.out.println("Aryan Sharma");
+        Scanner sc = new Scanner(System.in);
+        int sum = 0, rem;
+        int t = sc.nextInt();
+        for(int i = 0;i<=t ;i++)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[] =new int[n];
+            for(int j = 0;j<=n; j++)
+            {
+                arr[i] = sc.nextInt();
+                sum += arr[j];
+            }
+            rem = sum % m;
+            System.out.println(rem);
+        }
     }
 }
